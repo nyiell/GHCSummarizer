01803d9d290569d997076aeee163560579212114
nyiell <116977151+nyiell@users.noreply.github.com>
2023-02-15 14:34:58 -0500

    Optimize imports

--- BlogProcessor/RSSProcessor.py
+++ BlogProcessor/RSSProcessor.py
@@ -4,7 +4,6 @@ import regex as re
 import sqlite3
 import sys
 import logging
-from gensim.summarization import summarize
 
 logging.basicConfig(level=logging.INFO,format='%(asctime)s %(levelname)s %(message)s', datefmt='%Y-%m-%d %H:%M:%S', filename = 'logger.log', filemode='w')
 
